7eeea81342da31dee0b3318c88222ac3f7c27fcc
Þorvarður Örn Einarsson <thorrik58@gmail.com>
2020-05-13 11:17:46 +0000

    mvc, static stylesheet, path, express

--- app.js
+++ app.js
@@ -5,26 +5,22 @@ const express = require("express");
 
 const app = express();
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+const path = require("path");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/add-product", (req, res, next) => {
-  console.log("Middleware");
-  res.send(
-    `<form action='/product' method='POST'>
-        <input type='text' name='title'>
-            <button type='submit'>Add product</button>
-        </input>
-    </form>`
-  );
-});
+// host static css stylesheet
+app.use(express.static(path.join(__dirname, "public")));
 
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
+app.use("/admin", adminRoutes);
 
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello</h1>");
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
