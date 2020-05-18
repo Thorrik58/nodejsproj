88a3f4d3eed9866fe273b9720ff4ebfb522da5b3
Þorvarður Örn Einarsson <thorrik58@gmail.com>
2020-05-18 21:09:31 +0000

    pug template engine, starting work on handlebars

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const exphbs = require("express-handlebars");
 
 const app = express();
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
 app.listen(3000);
